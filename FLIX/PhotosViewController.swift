ade5b2960691bb0638f4838a1401390100cf3012
user143130 <user143130@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-14 17:36:48 -0700

    Added a table View, so now it is easier to see the title and movie summary. git status

--- FLIX/PhotosViewController.swift
+++ FLIX/PhotosViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
-var posts: [[String: Any]] = []
+    var posts: [[String: Any]] = []
     override func viewDidLoad()
     {
         super.viewDidLoad()
-            tableView.delegate = self
-            tableView.dataSource = self
-    
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
@@ -43,22 +43,21 @@ var posts: [[String: Any]] = []
         // Do any additional setup after loading the view.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
-
