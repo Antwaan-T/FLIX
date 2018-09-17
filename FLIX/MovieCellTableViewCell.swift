842cd667d53b605b265ba672013eb2d378b6450f
user143130 <user143130@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-17 13:13:50 -0700

    Set up a table view and successfully requested images and movies from the api

---
+++ FLIX/MovieCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCellTableViewCell.swift
+//  FLIX
+//
+//  Created by user143130 on 9/16/18.
+//  Copyright © 2018 Antwaan ThomasMorgan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
