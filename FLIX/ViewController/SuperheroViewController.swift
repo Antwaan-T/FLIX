c3b30de5cd444ae49f475bcf6f0fa7a24273c9a5
user143130 <user143130@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-11-04 17:50:40 -0800

    finished collection view

--- FLIX/ViewController/SuperheroViewController.swift
+++ FLIX/ViewController/SuperheroViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class SuperheroViewController: UIViewController {
+class SuperheroViewController: UIViewController, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionview: UICollectionView!
+    var movies : [[String: Any]] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        collectionview.dataSource = self
+}
+            func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                return movies.count
+            }
+            
+            func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                let cell = collectionview.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+            }
+    func fetchmovies ()
+    {
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request)
+        { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error
+            {
+                print(error.localizedDescription)
+            } else if let data = data
+            {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.collectionview.reloadData()
+                //self.refreshcontrol.endRefreshing()
+                
+            }
+        }
+        task.resume()
+        
     }
+
+
+
+
+
+
+
+
+}
     
 
     /*
